76f63d49221c062342006e66b204ffaa6531028c
Kristian Amadeus <amadeus.kristian@gmail.com>
2023-12-22 20:19:54 +0700

    commit 2023-12-22__20h19m: learning closures

---
+++ closures.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func intSeq() func() int {
+	i := 0
+
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func main() {
+	nextInt := intSeq()
+
+	fmt.Println(nextInt())
+
+	newInts := intSeq()
+
+	fmt.Println(newInts())
+	fmt.Println(newInts())
+	fmt.Println(nextInt())
+	fmt.Println(newInts())
+	fmt.Println(newInts())
+	fmt.Println(newInts())
+	fmt.Println(nextInt())
+}
