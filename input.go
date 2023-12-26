8fceaaac476e1a11de6d9ebb96ee4acde158da9b
Kristian Amadeus <amadeus.kristian@gmail.com>
2023-12-26 11:57:40 +0700

    2023-12-26__11h57m: input.go scan method

--- input.go
+++ input.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 func main() {
 	fmt.Printf("Input text: ")
+
+	// The Scan method
+	var w1, w2, w3 string
+	n, err := fmt.Scan(&w1, &w2, &w3)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("number of items read: %d\n", n)
+	fmt.Printf("read text: %s %s %s-\n", w1, w2, w3)
 }
